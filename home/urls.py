f9ba7d8e67199644262580ed3dcd9f7e57e06c99
Asmi Adhikari <asmiadhikari5-8@gmail.com>
2025-01-03 17:22:52 +0545

    added login page

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name="home"),
+    path('login',views.login,name="login"),
 ]
--- mental_health/urls.py
+++ mental_health/urls.py
@@ -8,4 +8,5 @@ admin.site.index_title = "Welcome to Mental-health"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
+    path('/login',include('home.urls')),
 ]
